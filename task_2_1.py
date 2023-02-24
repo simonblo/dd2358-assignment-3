8266426f6a53e29a4922c738541802d5a063587d
Simon Blom <102825244+simonblo@users.noreply.github.com>
2023-02-24 18:52:59 +0100

    Completed task 2.1

---
+++ task_2_1.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+import numpy
+import pytest
+import timeit
+
+def test_jacobi_iteration_numpy():
+    n = 128
+    f = init_grid_list(n)
+    g = numpy.array(f)
+    for _ in range(1000):
+        f = jacobi_iteration_list(f, n)
+        g = jacobi_iteration_numpy(g, n)
+        assert numpy.allclose(f, g)
+
+def init_grid_list(n):
+    return init_grid_numpy(n).tolist()
+
+def init_grid_numpy(n):
+    return numpy.pad(numpy.random.random((n-2, n-2)), 1)
+
+def jacobi_iteration_list(f, n):
+    g = [[0.0] * n for _ in range(n)]
+    for i in range(1, n - 1):
+        for j in range(1, n - 1):
+            g[i][j] = 0.25 * (f[i][j+1] + f[i][j-1] + f[i+1][j] + f[i-1][j])
+    return g
+
+def jacobi_iteration_numpy(f, n):
+    g = numpy.zeros((n, n))
+    numpy.add(g[+1:-1, +1:-1], f[+1:-1, +2:  ], g[+1:-1, +1:-1])
+    numpy.add(g[+1:-1, +1:-1], f[+1:-1,   :-2], g[+1:-1, +1:-1])
+    numpy.add(g[+1:-1, +1:-1], f[+2:  , +1:-1], g[+1:-1, +1:-1])
+    numpy.add(g[+1:-1, +1:-1], f[  :-2, +1:-1], g[+1:-1, +1:-1])
+    numpy.multiply(g, 0.25, g)
+    return g
+
+if __name__ == "__main__":
+    fig, ax = plt.subplots(figsize=(12, 8), constrained_layout=True)
+    fig.supxlabel("Grid size (N×N)")
+    fig.supylabel("Iteration time (s)")
+    xticks = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+    yticks = [5e-6, 5e-5, 5e-4, 5e-3, 5e-2, 5e-1, 5e-0]
+    f = []
+    f.append((init_grid_list,  jacobi_iteration_list,  [], [], "List"))
+    f.append((init_grid_numpy, jacobi_iteration_numpy, [], [], "Numpy"))
+    for n in xticks:
+        k = len(f)
+        for i in range(k):
+            g = f[i][0](n)
+            for j in range(10):
+                print(f"\r{n:4d}: {(i+1):d}/{k:d} {(10*j):d}%", end="")
+                a = timeit.default_timer()
+                g = f[i][1](g, n)
+                b = timeit.default_timer()
+                f[i][2].append(n)
+                f[i][3].append(b - a)
+        print(f"\r{n:4d}: {k:d}/{k:d} 100%")
+    for v in f:
+        ax.scatter(v[2], v[3], label=v[4], alpha=0.25)
+    ax.grid()
+    ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.xlim(xticks[0], xticks[-1])
+    plt.ylim(yticks[0], yticks[-1])
+    plt.xticks(xticks, [f"{v:d}×{v:d}" for v in xticks])
+    plt.yticks(yticks, [f"{v:.6f}"     for v in yticks])
+    plt.minorticks_off()
+    plt.show()
